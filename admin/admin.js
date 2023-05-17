9a1983a43b84ddcb5b0f4b2636efa7e40d6c8d3a
pachostore <pachostore@gmail.com>
2023-05-16 22:25:40 -0500

    Cambios

---
+++ admin/admin.js
@@ -0,0 +1,8 @@
+function guardarAlmacenamientoLocal(llave, valor_a_guardar){
+    localStorage.setItem(llave, JSON.stringify(valor_a_guardar))
+}
+
+function obtenerAlmacenamientoLocal(llave){
+    const datos = JSON.parse(localStorage.getItem(llave))
+    return datos
+}
