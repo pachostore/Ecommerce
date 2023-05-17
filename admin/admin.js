2300bae60e69ff541e9fd182ab6f87b99e609a34
pachostore <pachostore@gmail.com>
2023-05-16 23:15:53 -0500

    Adicion en js y ccs

--- admin/admin.js
+++ admin/admin.js
@@ -6,3 +6,35 @@ function obtenerAlmacenamientoLocal(llave){
     const datos = JSON.parse(localStorage.getItem(llave))
     return datos
 }
+
+let productos = obtenerAlmacenamientoLocal('productos') || [];
+let mensaje = document.getElementById('mensaje')
+
+// Añadir un producto 
+const  añadirProducto = document.getElementById('productoAñadir')
+const añadirValor = document.getElementById('valorAñadir')
+const añadirExistencia = document.getElementById('existenciasAñadir')
+const añadirImagen = document.getElementById('añadeImagen')
+
+document.getElementById("botonAñadir").addEventListener("click", function (even){
+    event.preventDefault()
+    let productoAñadir = añadirProducto.value
+    let valorAñadir = añadirValor.value
+    let existenciasAñadir = añadirExistencia.value
+    let añadirImagen = añadirImagen.value 
+
+    let van = true
+
+    if (productoAñadir == '' || valorAñadir == '' || existenciasAñadir == '' ||
+        añadirImagen == ''){
+        mensaje.classList.add('llenarCampos')
+
+    }
+
+    productos.push ({
+        nombre: productoAñadir,
+        valor: valorAñadir,
+        exixtencias: existenciasAñadir,
+        urlImagen: añadirImagen
+    })
+})
